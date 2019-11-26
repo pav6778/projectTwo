8b3efaaea55b103518a1edc07717f6e2ccb69be4
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2019-11-26 18:48:06 -0500

    updated article api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,40 +2,40 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+//   app.get("/api/examples", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.json(dbExamples);
+//     });
+//   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+//   // Create a new example
+//   app.post("/api/examples", function(req, res) {
+//     db.Example.create(req.body).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
+//   // Delete an example by id
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+// };
 
 app.get("/api/articles", function(req, res) {
-  db.Blog.findall({}).then(function(blogDB) {
-    res.json(blogDB);
+  db.Articles.findall({}).then(function(articleDB) {
+    res.json(articleDB);
   });
 });
 
 
 
 app.post("/api/articles", function(req, res) {
-  db.Blog.create({
-    text: req.body.text,
-    complete: req.body.complete
-  }).then(function(blogDB){
-    res.json(blogDB);
+  db.Articles.create({
+    title: req.body.title,
+    body: req.body.body
+  }).then(function(articleDB){
+    res.json(articleDB);
   });
-});
+})};
