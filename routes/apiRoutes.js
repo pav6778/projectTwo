5d952573756b42ff8d3d7f7d0803e92ccfad6337
Michał Swoboda <michal.swoboda@outlook.com>
2019-11-29 22:54:29 -0500

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,65 +1,57 @@
 var db = require("../models");
 
-module.exports = function(app) {
-    // Get all examples
-    //   app.get("/api/examples", function(req, res) {
-    //     db.Example.findAll({}).then(function(dbExamples) {
-    //       res.json(dbExamples);
-    //     });
-    //   });
+module.exports = function (app) {
 
-    //   // Create a new example
-    //   app.post("/api/examples", function(req, res) {
-    //     db.Example.create(req.body).then(function(dbExample) {
-    //       res.json(dbExample);
-    //     });
-    //   });
-
-    //   // Delete an example by id
-    //   app.delete("/api/examples/:id", function(req, res) {
-    //     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-    //       res.json(dbExample);
-    //     });
-    //   });
-    // };
-
-    app.get("/api/articles", function(req, res) {
-        db.Articles.findall({}).then(function(articleDB) {
+    // Get all articles
+    app.get("/api/articles", function (req, res) {
+        db.Articles.findall({}).then(function (articleDB) {
+            res.json(articleDB);
+        });
+    });
+    // Get all of user's articles
+    app.get("/api/articles/:user", function (req, res) {
+        db.Articles.findall({}).then(function (articleDB) {
             res.json(articleDB);
         });
     });
 
-
-
-    app.post("/api/articles", function(req, res) {
-
-
-
-        return User.create({
-
-                givenName: req.body.givenName,
-                surName: req.body.surName,
-                phoneNumber: req.body.phoneNumber,
-                email: req.body.email,
-                ownerId: req.user.customData._id,
-                notes: req.body.notes
-
-            }, {
-                returning: true
-            })
-            .then(function(user) {
-                res.send(user);
-            })
-            .catch(function(err) {
-                res.send(err);
-            });;
-
-
-        db.Articles.create({
-            title: req.body.title,
-            body: req.body.body
-        }).then(function(articleDB) {
+    // Get article based on ID
+    app.get("/api/article/:id", function (req, res) {
+        db.Articles.findall({}).then(function (articleDB) {
             res.json(articleDB);
         });
-    })
+    });
+
+    // Delete article based on ID
+    app.delete("/api/article/:id", function (req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+            res.json(dbExample);
+        });
+    });
+};
+
+// Add article to database
+
+app.get("/api/articles/:user", function (req, res) {
+    db.Articles.findall({}).then(function (articleDB) {
+        res.json(articleDB);
+    });
+});
+
+// author: DataTypes.STRING,
+// title: DataTypes.STRING,
+// body: DataTypes.TEXT,
+// likes: DataTypes.INTEGER,
+// user_liked: DataTypes.STRING,
+// comments: DataTypes.STRING
+
+
+app.post("/api/articles", function (req, res) {
+    db.Articles.create({
+        title: req.body.title,
+        body: req.body.body
+    }).then(function (articleDB) {
+        res.json(articleDB);
+    });
+})
 };
