3acab92319766790b8b26357474a46eeddfb5db0
Michał Swoboda <michal.swoboda@outlook.com>
2019-11-30 16:38:23 -0500

    Update write.js

--- public/js/write.js
+++ public/js/write.js
@@ -1,10 +1,33 @@
-// This function does an API call to delete posts
-function deletePost(id) {
+console.log("is loaded")
+$('#summernote').summernote({
+    tabsize: 2,
+    height: 200
+});
+
+
+
+
+
+$("#article-button").on("click", function (event) {
+    console.log("clicked")
+    event.preventDefault();
+
+    var body = $('#summernote').summernote('code');
+    var title = $("#article-title").val().trim();
+
+    console.log(title);
+    console.log(body);
+
     $.ajax({
-            method: "DELETE",
-            url: "/api/posts/" + id
-        })
-        .then(function() {
-            getPosts(postCategorySelect.val());
+        method: "POST",
+        url: "/api/articles",
+        data: { title:title, body:body}
+    })
+        .then(function () {
+            console.log("sentreqest");
+            window.location.href = "/";
+
         });
-}
+})
+
+
