f240cc09bb11c218d4eadc7fae14812a43f57db7
Michał Swoboda <michal.swoboda@outlook.com>
2019-12-01 22:25:41 -0500

    html routes updated

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,8 +8,8 @@ module.exports = function (app) {
         db.Article.findAll({}).then(function (blogDB) {
 
             articles = blogDB.map(blogDB => blogDB.dataValues);
-            userName = "bongoCat" ;
-            // userName = null ;
+            // userName = "bongoCat" ;
+            userName = null ;
 
             res.render('pages/index', { userName,  id: 0,  articles: articles });
         });
